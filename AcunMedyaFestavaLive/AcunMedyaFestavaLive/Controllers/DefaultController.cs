357a618f532b4463b3db938bff60d9154dc56132
furkanh7 <furkanhilaloglu9@gmail.com>
2024-05-08 01:13:47 +0300

    Festival Projesi
    ASP.NET MVC mimarisini kullanarak geliştirilmiştir ve festivale ilişkin bilgilerin yönetimini, katılımcıların kaydını ve etkinliklerin planlanmasını kolaylaştırmayı amaçlar.

---
+++ AcunMedyaFestavaLive/AcunMedyaFestavaLive/Controllers/DefaultController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaFestavaLive.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        
+        }
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+
+        }
+        public PartialViewResult PartialFeature()
+        {
+            return PartialView();
+
+        }
+        public PartialViewResult PartialAbout()
+        {
+            return PartialView();
+
+        }
+        public PartialViewResult PartialMeetArtist()
+        {
+            return PartialView();
+
+        }
+        public PartialViewResult PartialEventSchedule()
+        {
+            return PartialView();
+
+        }
+        public PartialViewResult PartialTicket()
+        {
+            return PartialView();
+
+        }
+        public PartialViewResult PartialContact()
+        {
+            return PartialView();
+
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+
+        }
+    }
+}
